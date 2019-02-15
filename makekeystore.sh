f72806e2aafd9e6e2d6538f9bcb49d4de63f5811
tuantrannav <dinh.anh.tuan.tran@nav.no>
2019-02-15 12:29:17 +0100

    enable makekeystore

--- makekeystore.sh
+++ makekeystore.sh
@@ -2,17 +2,18 @@
 
 mkdir -p mockkeys
 cd mockkeys
+if [ ! -f cert.pem ]; then
+    openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 365 -nodes -subj '/CN=vtpmock'
 
-openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 365 -nodes -subj '/CN=vtpmock'
+    # local-host SSL
+    openssl pkcs12 -export -name localhost-ssl -in cert.pem -inkey key.pem -out serverkeystore.p12 -password pass:changeit
+    keytool -importkeystore -destkeystore keystore.jks -srckeystore serverkeystore.p12 -srcstoretype pkcs12 -alias localhost-ssl -storepass changeit -keypass changeit -srcstorepass changeit
 
-# local-host SSL
-openssl pkcs12 -export -name localhost-ssl -in cert.pem -inkey key.pem -out serverkeystore.p12 -password pass:changeit
-keytool -importkeystore -destkeystore keystore.jks -srckeystore serverkeystore.p12 -srcstoretype pkcs12 -alias localhost-ssl -storepass changeit -keypass changeit -srcstorepass changeit
+    # app-key (jwt uststeder bl.a. i mocken, vi bruker samme noekkel per naa):
+    openssl pkcs12 -export -name app-key -in cert.pem -inkey key.pem -out serverkeystore2.p12 -password pass:changeit
+    keytool -importkeystore -destkeystore keystore.jks -srckeystore serverkeystore2.p12 -srcstoretype pkcs12 -alias app-key -storepass changeit -keypass changeit -srcstorepass changeit
 
-# app-key (jwt uststeder bl.a. i mocken, vi bruker samme noekkel per naa):
-openssl pkcs12 -export -name app-key -in cert.pem -inkey key.pem -out serverkeystore2.p12 -password pass:changeit
-keytool -importkeystore -destkeystore keystore.jks -srckeystore serverkeystore2.p12 -srcstoretype pkcs12 -alias app-key -storepass changeit -keypass changeit -srcstorepass changeit
+    # truststore for SSL:
+    keytool -import -trustcacerts -alias localhost-ssl -file cert.pem -keystore truststore.jks -storepass changeit -noprompt
 
-# truststore for SSL:
-keytool -import -trustcacerts -alias localhost-ssl -file cert.pem -keystore truststore.jks -storepass changeit -noprompt
-
+fi
